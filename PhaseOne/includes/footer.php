2c043660f2f091b1f9bf7a0295893c528d7034b0
Aaron-Gregoire <aaronjemmett@gmail.com>
2026-02-17 18:19:55 -0500

    fixed my js linking

--- PhaseOne/includes/footer.php
+++ PhaseOne/includes/footer.php
@@ -18,7 +18,8 @@
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 <!-- load js -->
-<script src="js/scripts.js"></script>
+<script src="js/validation.js"></script>
 
 </body>
+
 </html>
